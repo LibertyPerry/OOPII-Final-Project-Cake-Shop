cab6afa6ed6e0c32237c8da4499b730f865bfcf1
LibertyPerry <77160908+LibertyPerry@users.noreply.github.com>
2021-07-15 11:55:01 -0600

    Update Final_Project_Cake_Shop.java

--- Final_Project_Cake_Shop.java
+++ Final_Project_Cake_Shop.java
@@ -1,29 +1,35 @@
-package application;
-
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.shape.Box;
+import javafx.scene.shape.Circle;
 
 
 public class Final_Project_Cake_Shop extends Application{
 	int cakeShape = 1; //1 = round, 2 = rectangle
 	int cakeSize = 1; // 1 = small, 2 = medium, 3 = large
 	int cakeTiers = 1; // Max 3 tiers
+	int cakeFlavor = 1; // 1 = Vanilla, 2 = Chocolate, 3 = Strawberry
 	double length = 0;
 	double width = 0;
 	double height = 0;
@@ -49,21 +55,27 @@ public class Final_Project_Cake_Shop extends Application{
 		Button shape = new Button("Shape");
 		Button size = new Button("Size");
 		Button tiers = new Button("Tiers");
+		Button flavor = new Button("Flavor");
+		Button decorate = new Button("Decorate");
 		Button updateCake = new Button("Update Cake");
+		Button finsh = new Button("Finsh & Get Price");
 		HBox menu = new HBox();
 		menu.setSpacing(10);
 		menu.setAlignment(Pos.CENTER_LEFT);
 		menu.getChildren().add(shape);
 		menu.getChildren().add(size);
 		menu.getChildren().add(tiers);
-		menu.getChildren().add(updateCake);	
+		menu.getChildren().add(flavor);
+		menu.getChildren().add(decorate);
+		menu.getChildren().add(updateCake);
+		menu.getChildren().add(finsh);
 		menu.setVisible(false);
 		borderpane.setBottom(menu);
 
 		//Make shape Buttons
 		Button shapeRound = new Button("Round Cake");
 		Button shapeRectangle = new Button("Rectangle Cake");
-		HBox shapeMenu = new HBox();
+		VBox shapeMenu = new VBox();
 		shapeMenu.getChildren().add(shapeRound);
 		shapeMenu.getChildren().add(shapeRectangle);
 		shapeMenu.setSpacing(5);
@@ -76,7 +88,7 @@ public class Final_Project_Cake_Shop extends Application{
 		Button small = new Button("Small");
 		Button medium = new Button("Medium");
 		Button large = new Button("Large");
-		HBox sizeMenu = new HBox();
+		VBox sizeMenu = new VBox();
 		sizeMenu.getChildren().addAll(small, medium, large);
 		sizeMenu.setSpacing(5);
 		sizeMenu.setAlignment(Pos.BOTTOM_RIGHT);
@@ -88,13 +100,41 @@ public class Final_Project_Cake_Shop extends Application{
 		Button oneTier = new Button("One");
 		Button twoTier = new Button("Two");
 		Button threeTier = new Button("Three");
-		HBox tierMenu = new HBox();
+		VBox tierMenu = new VBox();
 		tierMenu.getChildren().addAll(oneTier, twoTier, threeTier);
 		tierMenu.setSpacing(5);
 		tierMenu.setAlignment(Pos.BOTTOM_RIGHT);
 		tierMenu.setVisible(false);
 		stackpane.getChildren().add(tierMenu);
 		StackPane.setAlignment(tierMenu, Pos.BOTTOM_RIGHT);
+		
+		
+		
+		//Make Flavor Buttons
+		Button vanilla = new Button("Vanilla");
+		Button chocolate = new Button("Chocolate");
+		Button strawberry = new Button("Strawberry");
+		VBox flavorMenu = new VBox();
+		flavorMenu.getChildren().addAll(vanilla, chocolate, strawberry);
+		flavorMenu.setSpacing(5);
+		flavorMenu.setAlignment(Pos.BOTTOM_RIGHT);
+		flavorMenu.setVisible(false);
+		stackpane.getChildren().add(flavorMenu);
+		StackPane.setAlignment(flavorMenu, Pos.BOTTOM_RIGHT);
+		
+		//Make Decoration Buttons
+		Button frosting = new Button("Frosting");
+		Button trim = new Button("Trim");
+		Button backToMainMenu = new Button("Back to Menu");
+		VBox decorationMenu = new VBox();
+		decorationMenu.getChildren().addAll(frosting, trim, backToMainMenu);
+		decorationMenu.setSpacing(5);
+		decorationMenu.setAlignment(Pos.BOTTOM_LEFT);
+		decorationMenu.setVisible(false);
+		stackpane.getChildren().add(decorationMenu);
+		StackPane.setAlignment(decorationMenu, Pos.BOTTOM_LEFT);
+			//Make Frosting Buttons
+		
 		
 		//Event Handlers
 		name.setOnAction(e -> {
@@ -107,12 +147,12 @@ public class Final_Project_Cake_Shop extends Application{
 			pane.getChildren().add(Ordername);
 			menu.setVisible(true);
 		});
-			//Menu Buttons
+		//Menu Buttons
+			//Shape Buttons
 			shape.setOnAction(e -> {
 				menu.setVisible(false);
 				shapeMenu.setVisible(true);
 			});
-				//Shape Buttons
 				shapeRound.setOnAction(e -> {
 					cakeShape = 1;
 					shapeMenu.setVisible(false);
@@ -123,11 +163,11 @@ public class Final_Project_Cake_Shop extends Application{
 					shapeMenu.setVisible(false);
 					menu.setVisible(true);
 				});	
+			//Size Buttons
 			size.setOnAction(e -> {
 				menu.setVisible(false);
 				sizeMenu.setVisible(true);
 			});
-				//Size Buttons
 				small.setOnAction(e -> {
 					cakeSize = 1;
 					sizeMenu.setVisible(false);
@@ -143,11 +183,11 @@ public class Final_Project_Cake_Shop extends Application{
 					sizeMenu.setVisible(false);
 					menu.setVisible(true);
 				});
+			//Tier Buttons
 			tiers.setOnAction(e -> {
 					menu.setVisible(false);
 					tierMenu.setVisible(true);
 			});
-				//Tier Buttons
 				oneTier.setOnAction(e -> {
 					cakeTiers = 1;
 					tierMenu.setVisible(false);
@@ -163,7 +203,30 @@ public class Final_Project_Cake_Shop extends Application{
 					tierMenu.setVisible(false);
 					menu.setVisible(true);
 				});
+			//Flavor Buttons
+			flavor.setOnAction(e -> {
+				menu.setVisible(false);
+				flavorMenu.setVisible(true);
+			});
+				vanilla.setOnAction(e -> {
+					cakeFlavor = 1;
+					flavorMenu.setVisible(false);
+					menu.setVisible(true);
+				});
+				chocolate.setOnAction(e -> {
+					cakeFlavor = 2;
+					flavorMenu.setVisible(false);
+					menu.setVisible(true);
+				});
+				strawberry.setOnAction(e -> {
+					cakeFlavor = 3;
+					flavorMenu.setVisible(false);
+					menu.setVisible(true);
+				});
+				
 			updateCake.setOnAction(e -> {
+				stackpane.getChildren().remove(stackpane.lookup("cake"));
+				stackpane.getChildren().remove(stackpane.lookup("cake2"));
 				//Size
 				if(cakeSize == 1) {
 					length = 150;
@@ -174,7 +237,7 @@ public class Final_Project_Cake_Shop extends Application{
 				}else if(cakeSize == 2) {
 					length = 300;
 					width = 300;
-					height = 200;
+					height = 175;
 					radius = 100;
 					height2 = 100;
 				}else if(cakeSize == 3) {
@@ -186,27 +249,50 @@ public class Final_Project_Cake_Shop extends Application{
 				}
 				// Shape
 				if(cakeShape == 1) {
-					Cylinder cake = new Cylinder(radius, height2);
-					borderpane.setCenter(cake);
-					
+					Circle cake = new Circle(400, -200, radius);
+					Label tierOneLabel = new Label("Tier One", cake);
+					tierOneLabel.setContentDisplay(ContentDisplay.TOP);
+					tierOneLabel.setTextFill(Color.BLACK);
+					pane.getChildren().add(cake);
+					stackpane.getChildren().add(tierOneLabel);
+					StackPane.setAlignment(tierOneLabel, Pos.CENTER_LEFT);
+					//Tiers
+					if(cakeTiers >= 2) {
+						Circle cake2 = new Circle(400, -400, (radius / 2));
+						Label tierTwoLabel = new Label("Tier Two", cake2);
+						tierTwoLabel.setContentDisplay(ContentDisplay.TOP);
+						tierTwoLabel.setTextFill(Color.BLACK);
+						pane.getChildren().add(cake2);
+						stackpane.getChildren().add(tierTwoLabel);
+						StackPane.setAlignment(tierTwoLabel, Pos.CENTER);
+					}
+					if(cakeTiers == 3) {
+						Circle cake3 = new Circle(400, -600, (radius / 4));
+					}
 				}else if (cakeShape == 2) {
-					Box cake = new Box(width, height, length);
-					borderpane.setCenter(cake);
-					
+					Rectangle cake = new Rectangle(400, -200, width, height);
+					Label tierOneLabel = new Label("Tier One", cake);
+					tierOneLabel.setContentDisplay(ContentDisplay.TOP);
+					tierOneLabel.setTextFill(Color.BLACK);
+					pane.getChildren().add(cake);
+					borderpane.setCenter(tierOneLabel);
+					//Tiers
+					if(cakeTiers == 1) {
+						
+					}else if(cakeTiers == 2) {
+						
+					}else if(cakeTiers == 3) {
+						
+					}
 				}
-				//Tiers
-				if(cakeTiers == 1) {
-					
-				}else if(cakeTiers == 2) {
-					
-				}else if(cakeTiers == 3) {
-					
-				}
+				//Flavor
+				//Frosting
 				
 			});
 		borderpane.getChildren().add(pane);
 		stackpane.getChildren().add(borderpane);
-		Scene scene = new Scene(stackpane, 800, 400);
+		Scene scene = new Scene(stackpane, 800, 700);
+		scene.setFill(Color.CORNFLOWERBLUE);
 		primaryStage.setTitle("Cake Shop");
 		primaryStage.setScene(scene);
 		primaryStage.show();
